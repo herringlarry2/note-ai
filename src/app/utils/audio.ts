fd2075925feb31d75b0b19c5838444945887c807
William Newman <william.newman94@gmail.com>
2024-12-11 22:03:15 -0500

    happy with it

--- src/app/utils/audio.ts
+++ src/app/utils/audio.ts
@@ -1,15 +1,49 @@
-import { Midi } from "@tonejs/midi";
-import { PolySynth } from "tone";
+"use client";
+
+import { getTransport, PolySynth, Context, Ticks } from "tone";
+import { NoteJSON } from "../types/Midi";
+import { useEffect, useRef } from "react";
 
 
-export function playMidi(midi: Midi) {
-    const synth = new PolySynth().toDestination();
-    midi.tracks[0].notes.forEach((note) => {
-        synth.triggerAttackRelease(
-            note.name,
-            note.duration,
-            note.time,
-            note.velocity
-        );
-    });
+function convertTicksToSeconds(ticks: number) {
+    return Ticks(ticks).toSeconds();
 }
+
+async function playNotes(notes: NoteJSON[], synth: PolySynth) {
+    synth.releaseAll();
+    getTransport().stop();
+    getTransport().cancel(0);
+
+
+    notes.forEach((note, idx) => {
+        const durationSeconds = convertTicksToSeconds(note.durationTicks) / 4;
+        const startTimeSeconds = convertTicksToSeconds(note.ticks) / 8;
+        getTransport().scheduleOnce((time) => {
+            synth.triggerAttackRelease(
+                note.name,
+                durationSeconds, 
+                time + startTimeSeconds, 
+                note.velocity
+            );
+        }, startTimeSeconds);
+    });
+
+    getTransport().start();
+    
+}
+
+
+
+
+export function useAudio() {
+    const synth = useRef<PolySynth>();
+
+    useEffect(() => {
+        const localSynth = new PolySynth().toDestination();
+        synth.current = localSynth;
+    }, [])
+
+    return {playNotes: (notes: NoteJSON[]) => synth.current && playNotes(notes, synth.current)};
+}
+
+
