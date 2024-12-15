868a303c366471e32b89a6395a05338150b250c6
William Newman <william.newman94@gmail.com>
2024-12-14 22:27:37 -0500

    works nicely

--- src/app/(BigButton)/ContinuedSequence.tsx
+++ src/app/(BigButton)/ContinuedSequence.tsx
@@ -4,20 +4,18 @@ import { Dispatch, SetStateAction } from "react";
 function ContinuedSequenceItem({
     name,
     idea,
-    setIdea,
+    onSelect,
     selected,
 }: {
     name: number;
     idea: NoteJSON[];
-    setIdea: (idea: NoteJSON[]) => void;
+    onSelect: () => void;
     selected: boolean;
 }) {
     return (
         <button
             className={`px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-900 transition-colors duration-200 shadow-md border border-white ${selected ? "bg-zinc-900" : ""}`}
-            onClick={() => {
-                setIdea(idea);
-            }}
+            onClick={onSelect}
         >
             {name}
         </button>
@@ -26,22 +24,22 @@ function ContinuedSequenceItem({
 
 export default function ContinuedSequence({
     ideas,
-    setCurrentIdea,
-    currentIdea,
+    onSelect,
+    selectedIdx,
 }: {
     ideas: NoteJSON[][];
-    currentIdea: NoteJSON[] | null;
-    setCurrentIdea: Dispatch<SetStateAction<NoteJSON[] | null>>;
+    onSelect: (idx: number) => void;
+    selectedIdx: number;
 }) {
     return (
         <div className="flex flex-row gap-2">
             {ideas.map((idea, index) => (
                 <ContinuedSequenceItem
                     key={index}
-                    selected={currentIdea === idea}
+                    selected={selectedIdx === index}
                     name={index}
                     idea={idea}
-                    setIdea={() => setCurrentIdea(idea)}
+                    onSelect={() => onSelect(index)}
                 />
             ))}
         </div>
