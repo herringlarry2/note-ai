afca5688a004925e370d0adbc55c4ccd5da42685
William Newman <william.newman94@gmail.com>
2024-12-14 22:18:40 -0500

    ran prettier...

--- src/app/(BigButton)/BigButton.tsx
+++ src/app/(BigButton)/BigButton.tsx
@@ -9,17 +9,22 @@ import { useAudio } from "../utils/audio";
 import ContinuedSequence from "./ContinuedSequence";
 import { PianoRoll } from "../(PianoRoll)/PianoRoll";
 
-
-function useMidiNotes(currentIdea: NoteJSON[] | null, originalMidiNotes: NoteJSON[] | null) {
+function useMidiNotes(
+    currentIdea: NoteJSON[] | null,
+    originalMidiNotes: NoteJSON[] | null
+) {
     const [midiNotes, setMidiNotes] = useState<NoteJSON[]>([]);
 
     useEffect(() => {
         if (currentIdea || originalMidiNotes) {
-            setMidiNotes([...(currentIdea ?? []), ...(originalMidiNotes ?? [])]);
+            setMidiNotes([
+                ...(currentIdea ?? []),
+                ...(originalMidiNotes ?? []),
+            ]);
         }
     }, [currentIdea, originalMidiNotes]);
 
-    return {midiNotes, setMidiNotes};
+    return { midiNotes, setMidiNotes };
 }
 
 function useFetchTracks() {
@@ -28,7 +33,9 @@ function useFetchTracks() {
     useEffect(() => {
         async function fetchTrack() {
             console.log("Fetching track");
-            const {signedUrl} = await noteClient.get<{signedUrl: string}>("/api/sign_s3");
+            const { signedUrl } = await noteClient.get<{ signedUrl: string }>(
+                "/api/sign_s3"
+            );
             const midi = await loadMidi(signedUrl);
             setTracks(midi.tracks);
         }
@@ -42,64 +49,77 @@ function BigButton() {
     // I think that ultimately we just just use the tonejs midi format but for now
     // I just want to see that it works so we'll massage temp into that format
     const [ideas, setIdeas] = useState<NoteJSON[][]>([]);
-    const {playNotes} = useAudio();
+    const { playNotes } = useAudio();
 
-    const tracks = useFetchTracks()
-
+    const tracks = useFetchTracks();
 
     const [currentIdea, setCurrentIdea] = useState<NoteJSON[] | null>(null);
 
-    const {midiNotes, setMidiNotes} = useMidiNotes(currentIdea, tracks?.[0]?.notes);
+    const { midiNotes, setMidiNotes } = useMidiNotes(
+        currentIdea,
+        tracks?.[0]?.notes
+    );
 
     function onClear() {
         setMidiNotes([]);
     }
 
-    console.log(midiNotes);
-
     async function onClickGenerate() {
-        const ideas = await noteClient.post<NoteJSON[][]>("/api/continue_track");
+        // Save the midi first until we have a better way to do this
+        await noteClient.post("/api/save_track", { notes: midiNotes });
+        const ideas = await noteClient.post<NoteJSON[][]>(
+            "/api/continue_track"
+        );
         setIdeas(ideas);
     }
-
 
     async function onClickPlay() {
         playNotes(midiNotes);
     }
 
     async function onClickSave() {
-        await noteClient.post("/api/save_track", {notes: midiNotes});
+        await noteClient.post("/api/save_track", { notes: midiNotes });
     }
 
-
     return (
-        <div className="flex flex-col gap-4">
-            <button
-                className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
-                onClick={onClickGenerate}
-            >
-                Click me for song
-            </button>
-            <button
-                className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
-                onClick={onClickPlay}
-            >
-                Play current idea
-            </button>
-            <button
-                className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
-                onClick={onClickSave}
-            >
-                Save
-            </button>
-            <button
-                className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
-                onClick={onClear}
-            >
-                Clear
-            </button>
-            <ContinuedSequence ideas={ideas} setCurrentIdea={setCurrentIdea} />
-            <PianoRoll width={1250} height={500} notes={midiNotes} setNotes={setMidiNotes} />
+        <div className="flex flex-col gap-4 items-center">
+            <ContinuedSequence
+                ideas={ideas}
+                setCurrentIdea={setCurrentIdea}
+                currentIdea={currentIdea}
+            />
+            <PianoRoll
+                width={1250}
+                height={500}
+                notes={midiNotes}
+                setNotes={setMidiNotes}
+            />
+            <div className="flex flex-row gap-2">
+                <button
+                    className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
+                    onClick={onClickGenerate}
+                >
+                    Generate
+                </button>
+                <button
+                    className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
+                    onClick={onClickPlay}
+                >
+                    Play
+                </button>
+                <button
+                    className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
+                    onClick={onClickSave}
+                >
+                    Save
+                </button>
+                <button
+                    className="px-6 py-3 bg-black text-white font-semibold rounded-full border border-white"
+                    onClick={onClear}
+                >
+                    Clear
+                </button>
+            </div>
         </div>
     );
 }
--- src/app/(BigButton)/ContinuedSequence.tsx
+++ src/app/(BigButton)/ContinuedSequence.tsx
@@ -1,21 +1,49 @@
 import { NoteJSON } from "../types/Midi";
 import { Dispatch, SetStateAction } from "react";
 
-
-function ContinuedSequenceItem({name, idea, setIdea}: {name: number, idea: NoteJSON[], setIdea: (idea: NoteJSON[]) => void}) {
+function ContinuedSequenceItem({
+    name,
+    idea,
+    setIdea,
+    selected,
+}: {
+    name: number;
+    idea: NoteJSON[];
+    setIdea: (idea: NoteJSON[]) => void;
+    selected: boolean;
+}) {
     return (
-        <button 
-            className="px-4 py-2 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition-colors duration-200 shadow-md"
-            onClick={() => {setIdea(idea)}}
+        <button
+            className={`px-4 py-2 bg-black text-white rounded-lg hover:bg-gray-900 transition-colors duration-200 shadow-md border border-white ${selected ? "bg-zinc-900" : ""}`}
+            onClick={() => {
+                setIdea(idea);
+            }}
         >
             {name}
         </button>
-    )
+    );
 }
 
-
-export default function ContinuedSequence({ideas, setCurrentIdea}: {ideas: NoteJSON[][], setCurrentIdea: Dispatch<SetStateAction<NoteJSON[] | null>>}) {
-    return <div>
-        {ideas.map((idea, index) => <ContinuedSequenceItem key={index} name={index} idea={idea} setIdea={() => setCurrentIdea(idea)} />)}
-    </div>
+export default function ContinuedSequence({
+    ideas,
+    setCurrentIdea,
+    currentIdea,
+}: {
+    ideas: NoteJSON[][];
+    currentIdea: NoteJSON[] | null;
+    setCurrentIdea: Dispatch<SetStateAction<NoteJSON[] | null>>;
+}) {
+    return (
+        <div className="flex flex-row gap-2">
+            {ideas.map((idea, index) => (
+                <ContinuedSequenceItem
+                    key={index}
+                    selected={currentIdea === idea}
+                    name={index}
+                    idea={idea}
+                    setIdea={() => setCurrentIdea(idea)}
+                />
+            ))}
+        </div>
+    );
 }
